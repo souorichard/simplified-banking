083ef7be28a004977cd61cc9bb480b93fcd136b8
souorichard <rodrigues.lpta@gmail.com>
2026-06-23 09:08:29 -0300

    feat: initial config

---
+++ src/main/java/com/simplifiedBanking/SimplifiedBankingApplication.java
@@ -0,0 +1,13 @@
+package com.simplifiedBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedBankingApplication.class, args);
+	}
+
+}
